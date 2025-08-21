69430f7d82bd429a5386ac7f44f1ff084c86d920
Pageblan <wanjogudk@gmail.com>
2025-08-21 15:27:15 +0300

    Remove tracked build artifacts and add .gitignore

--- dist/services/approvalService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAllApprovals = void 0;
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-const getAllApprovals = async () => {
-    return prisma.approval.findMany();
-};
-exports.getAllApprovals = getAllApprovals;
